a9e29aa21fbcb42987238ce6a9fa0d32fc621884
Sidnei Santos Mendes <sidnei1987@gmail.com>
2022-09-15 15:34:30 -0300

    Rotas de  entra e cadastrar criadas

---
+++ src/app/model/postagem.ts
@@ -0,0 +1,12 @@
+import { Tema } from "./tema"
+import { User } from "./user"
+
+export class Postagem{
+
+ public id :number
+ public titulo: string
+ public texto: string
+ public date: Date
+ public usuario: User
+ public tema: Tema
+}
---
+++ src/app/model/user.ts
@@ -0,0 +1,12 @@
+import { Postagem } from "./postagem"
+
+export class User{
+  public idUsuario: number
+  public nome: string
+  public email: string
+  public senha: string
+  public postagem: Postagem []
+}
+
+
+
